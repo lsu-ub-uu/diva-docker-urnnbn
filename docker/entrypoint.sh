e6d5c41d3b3efce02a29e78077e9004c2888ca9c
perebartrolisimo <pere.bartroli@ub.uu.se>
2026-06-16 13:30:57 +0000

    docker starts with diva service

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 
+mkdir /usr/local/tomcat/webapps/urn-nbn
+cd /usr/local/tomcat/webapps/urn-nbn
+jar -xvf /usr/local/tomcat/webapps/urn-nbn.war
+sed -i "s|{SERVICE_NAME}|diva|g" /usr/local/tomcat/webapps/urn-nbn/WEB-INF/web.xml
+rm -f /usr/local/tomcat/webapps/urn-nbn.war
+
 /usr/local/tomcat/bin/catalina.sh run &
 TOMCAT_PID=$!
 
@@ -8,6 +14,5 @@ until [ -d /usr/local/tomcat/webapps/urn-nbn ]; do
   sleep 1
 done
 
-sed -i "s|{SERVICE_NAME}|diva|g" /usr/local/tomcat/webapps/urn-nbn/WEB-INF/web.xml
 cp -rf /tmp/urn-nbn/* /usr/local/tomcat/webapps/urn-nbn/
 wait "$TOMCAT_PID"
